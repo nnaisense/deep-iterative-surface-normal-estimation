ba52c76006305de3981ee9546aeee5263d0910a9
Jan Eric Lenssen <janeric.lenssen@tu-dortmund.de>
2022-08-01 14:22:31 +0200

    Fix for newer Pytorch versions.

--- cuda/quat_to_mat.cpp
+++ cuda/quat_to_mat.cpp
@@ -1,6 +1,6 @@
 #include <torch/torch.h>
 
-#define IS_CUDA(x) AT_ASSERTM(x.type().is_cuda(), #x " must be CUDA tensor");
+#define IS_CUDA(x) AT_ASSERTM(x.is_cuda(), #x " must be CUDA tensor");
 #define IS_CONTIGUOUS(x) AT_ASSERTM(x.is_contiguous(), #x " is not contiguous");
 #define CHECK_INPUT(x) IS_CUDA(x) IS_CONTIGUOUS(x)
 
